fe1ff1f2e9a39cc8f5e8cd5f50132c76e68005ff
softwarecorridor <softwarecorridor@users.noreply.github.com>
2022-03-14 17:24:22 -0400

    read in text box on button press

--- app/src/main/java/com/softwarecorridor/githubtimelinedemo/FirstFragment.kt
+++ app/src/main/java/com/softwarecorridor/githubtimelinedemo/FirstFragment.kt
@@ -1,17 +1,21 @@
 package com.softwarecorridor.githubtimelinedemo
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.softwarecorridor.githubtimelinedemo.databinding.FragmentFirstBinding
+
+
+private const val TAG = "FirstFragment"
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,8 +28,20 @@ class FirstFragment : Fragment() {
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
 
+        binding.buttonFirst.setOnClickListener { view: View? ->
+
+            val textBox: EditText? = this.activity?.findViewById(R.id.et_user_input)
+            if (textBox != null) {
+                val userInput = textBox.text
+                Log.d(TAG, "string: $userInput")
+            } else {
+                Log.d(TAG, "text box is null for some reason")
+            }
+
+        }
+
+        return binding.root
     }
 
     override fun onDestroyView() {
